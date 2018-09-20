347fda16896aa3e28bd95b2c0c911a7be16e846f
jaywcjlove <398188662@qq.com>
2018-09-20 23:40:46 +0800

    released v1.0.14

---
+++ dist/cookie.esm.js
@@ -0,0 +1,142 @@
+/*!
+ * cookiejs v1.0.13
+ * Change the cookie library a simple API provides
+ * 
+ * Copyright (c) 2018 kenny wang
+ * https://github.com/jaywcjlove/cookie.js
+ * 
+ * Licensed under the MIT license.
+ */
+
+function _typeof(obj) {
+  if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+    _typeof = function (obj) {
+      return typeof obj;
+    };
+  } else {
+    _typeof = function (obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+    };
+  }
+
+  return _typeof(obj);
+}
+
+var _this = undefined,
+    _arguments = arguments;
+
+function getKeys(obj) {
+  var names = [],
+      name = '';
+
+  for (name in obj) {
+    if (obj.hasOwnProperty(name)) names.push(name);
+  }
+
+  return names;
+}
+
+function isPlainObject(value) {
+  return !!value && Object.prototype.toString.call(value) === '[object Object]';
+}
+
+function isArray(value) {
+  return value instanceof Array;
+}
+
+function toArray(value) {
+  return Array.prototype.slice.call(value);
+}
+
+function Cookie() {
+  if (!(this instanceof Cookie)) {
+    return new Cookie();
+  }
+}
+
+Cookie.prototype = {
+  get: function get(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';'); //把cookie分割成组    
+
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i]; //取得字符串    
+
+      while (c.charAt(0) == ' ') {
+        //判断一下字符串有没有前导空格    
+        c = c.substring(1, c.length); //有的话，从第二位开始取    
+      } //如果含有我们要的name
+
+
+      if (c.indexOf(nameEQ) == 0) {
+        return decodeURI(c.substring(nameEQ.length, c.length)); //解码并截取我们要值    
+      }
+    }
+
+    return false;
+  },
+  set: function set(name, value, options) {
+    if (isPlainObject(name)) {
+      for (var k in name) {
+        if (name.hasOwnProperty(k)) _this.set(k, name[k], value);
+      }
+    } else {
+      var opt = isPlainObject(options) ? options : {
+        expires: options
+      },
+          expires = opt.expires !== undefined ? opt.expires : '',
+          expiresType = _typeof(expires),
+          path = opt.path !== undefined ? ';path=' + opt.path : ';path=/',
+          domain = opt.domain ? ';domain=' + opt.domain : '',
+          secure = opt.secure ? ';secure' : ''; //过期时间
+
+
+      if (expiresType === 'string' && expires !== '') expires = new Date(expires);else if (expiresType === 'number') expires = new Date(+new Date() + 1000 * 60 * 60 * 24 * expires);
+      if (expires !== '' && 'toGMTString' in expires) expires = ';expires=' + expires.toGMTString();
+      document.cookie = name + "=" + encodeURI(value) + expires + path + domain + secure; //转码并赋值    
+    }
+  },
+  remove: function remove(names) {
+    names = isArray(names) ? names : toArray(_arguments);
+
+    for (var i = 0, l = names.length; i < l; i++) {
+      _this.set(names[i], '', -1);
+    }
+
+    return names;
+  },
+  clear: function clear(name) {
+    return name ? _this.remove(name) : _this.remove(getKeys(_this.all()));
+  },
+  all: function all() {
+    if (document.cookie === '') return {};
+    var cookies = document.cookie.split('; '),
+        result = {};
+
+    for (var i = 0, l = cookies.length; i < l; i++) {
+      var item = cookies[i].split('=');
+      result[decodeURI(item[0])] = decodeURI(item[1]);
+    }
+
+    return result;
+  }
+};
+var _Cookie = null;
+
+var cookie = function cookie(name, value, options) {
+  var argm = _arguments;
+  if (!_Cookie) _Cookie = Cookie();
+  if (argm.length === 0) return _Cookie.all();
+  if (argm.length === 1 && name === null) return _Cookie.clear();
+  if (argm.length === 2 && !value) return _Cookie.clear(name);
+  if (typeof name == 'string' && !value) return _Cookie.get(name);
+  if (isPlainObject(name) || argm.length > 1 && name && value) return _Cookie.set(name, value, options);
+  if (value === null) return _Cookie.remove(name);
+  return _Cookie.all();
+};
+
+for (var a in Cookie.prototype) {
+  cookie[a] = Cookie.prototype[a];
+}
+
+export default cookie;
--- dist/cookie.js
+++ dist/cookie.js
@@ -2,116 +2,149 @@
  * cookiejs v1.0.13
  * Change the cookie library a simple API provides
  * 
- * Copyright (c) 2017 kenny wang
+ * Copyright (c) 2018 kenny wang
  * https://github.com/jaywcjlove/cookie.js
  * 
  * Licensed under the MIT license.
  */
-(function(f) {
-    if (typeof exports === "object" && typeof module !== "undefined") {
-        module.exports = f();
-    } else if (typeof define === "function" && define.amd) {
-        define([], f);
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.cookie = factory());
+}(this, (function () { 'use strict';
+
+  function _typeof(obj) {
+    if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+      _typeof = function (obj) {
+        return typeof obj;
+      };
     } else {
-        var g;
-        if (typeof window !== "undefined") {
-            g = window;
-        } else if (typeof global !== "undefined") {
-            g = global;
-        } else if (typeof self !== "undefined") {
-            g = self;
-        } else {
-            g = this;
+      _typeof = function (obj) {
+        return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+      };
+    }
+
+    return _typeof(obj);
+  }
+
+  var _this = undefined,
+      _arguments = arguments;
+
+  function getKeys(obj) {
+    var names = [],
+        name = '';
+
+    for (name in obj) {
+      if (obj.hasOwnProperty(name)) names.push(name);
+    }
+
+    return names;
+  }
+
+  function isPlainObject(value) {
+    return !!value && Object.prototype.toString.call(value) === '[object Object]';
+  }
+
+  function isArray(value) {
+    return value instanceof Array;
+  }
+
+  function toArray(value) {
+    return Array.prototype.slice.call(value);
+  }
+
+  function Cookie() {
+    if (!(this instanceof Cookie)) {
+      return new Cookie();
+    }
+  }
+
+  Cookie.prototype = {
+    get: function get(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';'); //把cookie分割成组    
+
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]; //取得字符串    
+
+        while (c.charAt(0) == ' ') {
+          //判断一下字符串有没有前导空格    
+          c = c.substring(1, c.length); //有的话，从第二位开始取    
+        } //如果含有我们要的name
+
+
+        if (c.indexOf(nameEQ) == 0) {
+          return decodeURI(c.substring(nameEQ.length, c.length)); //解码并截取我们要值    
         }
-        g.cookie = f();
-    }
-})(function() {
-    var define, module, exports;
-    function getKeys(obj) {
-        var names = [], name = "";
-        for (name in obj) {
-            if (obj.hasOwnProperty(name)) names.push(name);
+      }
+
+      return false;
+    },
+    set: function set(name, value, options) {
+      if (isPlainObject(name)) {
+        for (var k in name) {
+          if (name.hasOwnProperty(k)) _this.set(k, name[k], value);
         }
-        return names;
-    }
-    function isPlainObject(value) {
-        return !!value && Object.prototype.toString.call(value) === "[object Object]";
-    }
-    function isArray(value) {
-        return value instanceof Array;
-    }
-    function toArray(value) {
-        return Array.prototype.slice.call(value);
-    }
-    function Cookie() {
-        if (!(this instanceof Cookie)) {
-            return new Cookie();
-        }
-    }
-    Cookie.prototype = {
-        get: function(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(";");
-            //把cookie分割成组    
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                //取得字符串    
-                while (c.charAt(0) == " ") {
-                    //判断一下字符串有没有前导空格    
-                    c = c.substring(1, c.length);
-                }
-                //如果含有我们要的name
-                if (c.indexOf(nameEQ) == 0) {
-                    return decodeURI(c.substring(nameEQ.length, c.length));
-                }
-            }
-            return false;
+      } else {
+        var opt = isPlainObject(options) ? options : {
+          expires: options
         },
-        set: function(name, value, options) {
-            if (isPlainObject(name)) {
-                for (var k in name) {
-                    if (name.hasOwnProperty(k)) this.set(k, name[k], value);
-                }
-            } else {
-                var opt = isPlainObject(options) ? options : {
-                    expires: options
-                }, expires = opt.expires !== undefined ? opt.expires : "", expiresType = typeof expires, path = opt.path !== undefined ? ";path=" + opt.path : ";path=/", domain = opt.domain ? ";domain=" + opt.domain : "", secure = opt.secure ? ";secure" : "";
-                //过期时间
-                if (expiresType === "string" && expires !== "") expires = new Date(expires); else if (expiresType === "number") expires = new Date(+new Date() + 1e3 * 60 * 60 * 24 * expires);
-                if (expires !== "" && "toGMTString" in expires) expires = ";expires=" + expires.toGMTString();
-                document.cookie = name + "=" + encodeURI(value) + expires + path + domain + secure;
-            }
-        },
-        remove: function(names) {
-            names = isArray(names) ? names : toArray(arguments);
-            for (var i = 0, l = names.length; i < l; i++) {
-                this.set(names[i], "", -1);
-            }
-            return names;
-        },
-        clear: function(name) {
-            return name ? this.remove(name) : this.remove(getKeys(this.all()));
-        },
-        all: function() {
-            if (document.cookie === "") return {};
-            var cookies = document.cookie.split("; "), result = {};
-            for (var i = 0, l = cookies.length; i < l; i++) {
-                var item = cookies[i].split("=");
-                result[decodeURI(item[0])] = decodeURI(item[1]);
-            }
-            return result;
-        }
-    };
-    var cookie = function(name, value, options) {
-        var argm = arguments;
-        if (argm.length === 0) return Cookie().all();
-        if (argm.length === 1 && name === null) return Cookie().clear();
-        if (argm.length === 2 && !value) return Cookie().clear(name);
-        if (typeof name == "string" && !value) return Cookie().get(name);
-        if (isPlainObject(name) || argm.length > 1 && name && value) return Cookie().set(name, value, options);
-        if (value === null) return Cookie().remove(name);
-        return Cookie().all();
-    };
-    for (var a in Cookie.prototype) cookie[a] = Cookie.prototype[a];
-    return cookie;
-});
+            expires = opt.expires !== undefined ? opt.expires : '',
+            expiresType = _typeof(expires),
+            path = opt.path !== undefined ? ';path=' + opt.path : ';path=/',
+            domain = opt.domain ? ';domain=' + opt.domain : '',
+            secure = opt.secure ? ';secure' : ''; //过期时间
+
+
+        if (expiresType === 'string' && expires !== '') expires = new Date(expires);else if (expiresType === 'number') expires = new Date(+new Date() + 1000 * 60 * 60 * 24 * expires);
+        if (expires !== '' && 'toGMTString' in expires) expires = ';expires=' + expires.toGMTString();
+        document.cookie = name + "=" + encodeURI(value) + expires + path + domain + secure; //转码并赋值    
+      }
+    },
+    remove: function remove(names) {
+      names = isArray(names) ? names : toArray(_arguments);
+
+      for (var i = 0, l = names.length; i < l; i++) {
+        _this.set(names[i], '', -1);
+      }
+
+      return names;
+    },
+    clear: function clear(name) {
+      return name ? _this.remove(name) : _this.remove(getKeys(_this.all()));
+    },
+    all: function all() {
+      if (document.cookie === '') return {};
+      var cookies = document.cookie.split('; '),
+          result = {};
+
+      for (var i = 0, l = cookies.length; i < l; i++) {
+        var item = cookies[i].split('=');
+        result[decodeURI(item[0])] = decodeURI(item[1]);
+      }
+
+      return result;
+    }
+  };
+  var _Cookie = null;
+
+  var cookie = function cookie(name, value, options) {
+    var argm = _arguments;
+    if (!_Cookie) _Cookie = Cookie();
+    if (argm.length === 0) return _Cookie.all();
+    if (argm.length === 1 && name === null) return _Cookie.clear();
+    if (argm.length === 2 && !value) return _Cookie.clear(name);
+    if (typeof name == 'string' && !value) return _Cookie.get(name);
+    if (isPlainObject(name) || argm.length > 1 && name && value) return _Cookie.set(name, value, options);
+    if (value === null) return _Cookie.remove(name);
+    return _Cookie.all();
+  };
+
+  for (var a in Cookie.prototype) {
+    cookie[a] = Cookie.prototype[a];
+  }
+
+  return cookie;
+
+})));
