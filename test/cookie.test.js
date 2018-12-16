916949c9d803a80302434b420ec7bcdc9c9b960a
jaywcjlove <398188662@qq.com>
2018-12-17 02:38:15 +0800

    test: Add clean all cookie test caset.

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -1,6 +1,6 @@
 const cookie = require('../dist/cookie');
 
-test('set cookie', () => {
+test('Set cookie', () => {
   expect(cookie("test1", "value1", 1800)).toBeUndefined();
   expect(cookie("test2", "value2")).toBeUndefined();
   expect(cookie.set({ "test3": "value3", "test4": "value4" })).toBeUndefined();
@@ -8,7 +8,7 @@ test('set cookie', () => {
   expect(cookie({ "test6": "value6", "test7": "value7" })).toBeUndefined();
 });
 
-test('get cookie', () => {
+test('Get cookie', () => {
   expect(cookie('test1')).toBe('value1');
   expect(cookie('test2')).toBe('value2');
   expect(cookie('test3')).toBe('value3');
@@ -18,7 +18,7 @@ test('get cookie', () => {
   expect(cookie('test7')).toBe('value7');
 });
 
-test('get all cookie', () => {
+test('Get all cookie', () => {
   const json = { "test1": "value1", "test2": "value2", "test3": "value3", "test4": "value4", "test5": "value5", "test6": "value6", "test7": "value7" }
   expect(cookie()).toEqual(json);
   expect(cookie.all()).toEqual(json);
@@ -30,9 +30,15 @@ test('Delete one cookie', () => {
   expect(cookie.remove('test2')).toEqual(["test2"]);
 });
 
-
 test('Delete multiple cookies', () => {
   expect(cookie.remove('test3', 'test4')).toEqual(["test3", "test4"]);
   expect(cookie('test3')).toBe(false);
   expect(cookie('test4')).toBe(false);
+});
+
+test('Clean all cookie', () => {
+  expect(cookie.clear()).toEqual(["test5", "test6", "test7"]);
+  expect(cookie('test5')).toBe(false);
+  expect(cookie('test6')).toBe(false);
+  expect(cookie('test7')).toBe(false);
 });
